18ee6e7b71c1bc15afa426eac43732e24a9cd4b8
Grate <531129143@qq.com>
2019-05-11 21:03:04 +0800

    hhh
    Signed-off-by: Grate <531129143@qq.com>

--- app/src/main/java/com/example/teclearner_10/biologyActivity.java
+++ app/src/main/java/com/example/teclearner_10/biologyActivity.java
@@ -8,6 +8,6 @@ public class biologyActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_biologyac);
+            setContentView(R.layout.activity_biologyac);
     }
 }
