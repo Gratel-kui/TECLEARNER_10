d064a0794b3e2067beb21bc0e0aae4e39f55dbaa
Grate <531129143@qq.com>
2019-05-27 16:21:48 +0800

    hhh
    Signed-off-by: Grate <531129143@qq.com>

--- app/src/main/java/com/example/teclearner_10/biologyActivity.java
+++ app/src/main/java/com/example/teclearner_10/biologyActivity.java
@@ -1,14 +1,33 @@
 package com.example.teclearner_10;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 
-public class biologyActivity extends AppCompatActivity {
+public class biologyActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_biologyac);
+        setContentView(R.layout.activity_biologyac);
+
+        init();
+    }
+
+    private void init() {
+        findViewById(R.id.text_digest).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.text_digest:
+                Intent intent1 = new Intent();
+                intent1.setClass(getApplicationContext(), digestActivity.class);
+                this.startActivity(intent1);
+                break;
+        }
     }
 }
